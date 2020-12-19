5406e9915e32a7d7d93c5c3f464e264ab032ffc7
Jarkko Mourujarvi <jarkkom@unity3d.com>
2020-12-19 14:51:15 +0200

    Day 19 part 2

--- src/19/part2.rs
+++ src/19/part2.rs
@@ -101,8 +101,12 @@ fn read_input(reader: impl Read) -> Result<(HashMap<usize, Rule>, Vec<String>), 
     Ok((rules, messages))
 }
 
-fn traverse_rules(rules: &HashMap<usize, Rule>, pos: usize) -> String {
+fn traverse_rules(rules: &HashMap<usize, Rule>, pos: usize, depth: usize) -> String {
     let mut regex = String::from("");
+
+    if depth > 20 {
+        return regex;
+    }
 
     match &rules[&pos] {
         Rule::Char(s) => {
@@ -110,15 +114,15 @@ fn traverse_rules(rules: &HashMap<usize, Rule>, pos: usize) -> String {
         }
         Rule::Concat(a) => {
             a.iter()
-                .for_each(|r| regex.push_str(&traverse_rules(rules, *r)));
+                .for_each(|r| regex.push_str(&traverse_rules(rules, *r, depth + 1)));
         }
         Rule::Union(a, b) => {
             regex.push('(');
             a.iter()
-                .for_each(|r| regex.push_str(&traverse_rules(rules, *r)));
+                .for_each(|r| regex.push_str(&traverse_rules(rules, *r, depth + 1)));
             regex.push('|');
             b.iter()
-                .for_each(|r| regex.push_str(&traverse_rules(rules, *r)));
+                .for_each(|r| regex.push_str(&traverse_rules(rules, *r, depth + 1)));
             regex.push(')');
         }
     }
@@ -136,7 +140,7 @@ fn main() {
 
     println!("rules = {:?}", inputs);
 
-    let regex_str = format!("^{}$", traverse_rules(&inputs.0, 0));
+    let regex_str = format!("^{}$", traverse_rules(&inputs.0, 0, 0));
     println!("regex = {}", regex_str);
 
     let regex = Regex::new(&regex_str).unwrap();
@@ -150,29 +154,127 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_input_reader() {
-        let test_input = r#"0: 4 1 5
-1: 2 3 | 3 2
-2: 4 4 | 5 5
-3: 4 5 | 5 4
-4: "a"
-5: "b"
+    fn test_non_recursive() {
+        let test_input = r#"42: 9 14 | 10 1
+9: 14 27 | 1 26
+10: 23 14 | 28 1
+1: "a"
+11: 42 31
+5: 1 14 | 15 1
+19: 14 1 | 14 14
+12: 24 14 | 19 1
+16: 15 1 | 14 14
+31: 14 17 | 1 13
+6: 14 14 | 1 14
+2: 1 24 | 14 4
+0: 8 11
+13: 14 3 | 1 12
+15: 1 | 14
+17: 14 2 | 1 7
+23: 25 1 | 22 14
+28: 16 1
+4: 1 1
+20: 14 14 | 1 15
+3: 5 14 | 16 1
+27: 1 6 | 14 18
+14: "b"
+21: 14 1 | 1 14
+25: 1 1 | 1 14
+22: 14 14
+8: 42
+26: 14 22 | 1 20
+18: 15 15
+7: 14 5 | 1 21
+24: 14 1
 
-ababbb
-bababa
-abbbab
-aaabbb
-aaaabbb"#;
+abbbbbabbbaaaababbaabbbbabababbbabbbbbbabaaaa
+bbabbbbaabaabba
+babbbbaabbbbbabbbbbbaabaaabaaa
+aaabbbbbbaaaabaababaabababbabaaabbababababaaa
+bbbbbbbaaaabbbbaaabbabaaa
+bbbababbbbaaaaaaaabbababaaababaabab
+ababaaaaaabaaab
+ababaaaaabbbaba
+baabbaaaabbaaaababbaababb
+abbbbabbbbaaaababbbbbbaaaababb
+aaaaabbaabaaaaababaa
+aaaabbaaaabbaaa
+aaaabbaabbaaaaaaabbbabbbaaabbaabaaa
+babaaabbbaaabaababbaabababaaab
+aabbbbbaabbbaaaaaabbbbbababaaaaabbaaabba"#;
         let output = read_input(test_input.as_bytes()).unwrap();
 
         println!("output = {:?}", output);
 
-        let regex_str = format!("^{}$", traverse_rules(&output.0, 0));
+        let regex_str = format!("^{}$", traverse_rules(&output.0, 0, 0));
         println!("regex = {}", regex_str);
 
         let regex = Regex::new(&regex_str).unwrap();
 
         let matching = output.1.iter().filter(|i| regex.is_match(i)).count();
-        assert_eq!(matching, 2);
+        assert_eq!(matching, 3);
     }
+
+    #[test]
+    fn test_recursive() {
+        let test_input = r#"42: 9 14 | 10 1
+9: 14 27 | 1 26
+10: 23 14 | 28 1
+1: "a"
+11: 42 31 | 42 11 31
+5: 1 14 | 15 1
+19: 14 1 | 14 14
+12: 24 14 | 19 1
+16: 15 1 | 14 14
+31: 14 17 | 1 13
+6: 14 14 | 1 14
+2: 1 24 | 14 4
+0: 8 11
+13: 14 3 | 1 12
+15: 1 | 14
+17: 14 2 | 1 7
+23: 25 1 | 22 14
+28: 16 1
+4: 1 1
+20: 14 14 | 1 15
+3: 5 14 | 16 1
+27: 1 6 | 14 18
+14: "b"
+21: 14 1 | 1 14
+25: 1 1 | 1 14
+22: 14 14
+8: 42 | 42 8
+26: 14 22 | 1 20
+18: 15 15
+7: 14 5 | 1 21
+24: 14 1
+
+abbbbbabbbaaaababbaabbbbabababbbabbbbbbabaaaa
+bbabbbbaabaabba
+babbbbaabbbbbabbbbbbaabaaabaaa
+aaabbbbbbaaaabaababaabababbabaaabbababababaaa
+bbbbbbbaaaabbbbaaabbabaaa
+bbbababbbbaaaaaaaabbababaaababaabab
+ababaaaaaabaaab
+ababaaaaabbbaba
+baabbaaaabbaaaababbaababb
+abbbbabbbbaaaababbbbbbaaaababb
+aaaaabbaabaaaaababaa
+aaaabbaaaabbaaa
+aaaabbaabbaaaaaaabbbabbbaaabbaabaaa
+babaaabbbaaabaababbaabababaaab
+aabbbbbaabbbaaaaaabbbbbababaaaaabbaaabba"#;
+        let output = read_input(test_input.as_bytes()).unwrap();
+
+        println!("output = {:?}", output);
+
+        let regex_str = format!("^{}$", traverse_rules(&output.0, 0, 0));
+        println!("regex = {}", regex_str);
+
+        let regex = Regex::new(&regex_str).unwrap();
+
+        let matching = output.1.iter().filter(|i| regex.is_match(i)).count();
+        assert_eq!(matching, 12);
+    }
+
 }
